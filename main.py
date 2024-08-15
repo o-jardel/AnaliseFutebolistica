c53a63a96630ded0554b0bc5d5c6f61abcdc5051
o-jardel <jardelferreira@ufpr.br>
2024-08-14 22:54:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from scipy.stats import chi2_contingency
 
 # Carregar dados
 dados = pd.read_csv('brasileirao_serie_a.csv')
@@ -41,19 +42,16 @@ dados_treino['resultado'] = dados_treino.apply(
 
 
 #primeiro botão
-if st.button("Gerar gráfico de resultados dos times mandantes"):
+if st.button("Gerar gráfico para calcular a proporção de V/E/D para cada time mandante."):
 
     # Contagem de resultados para times mandantes
     contagem_mandante = dados_treino.groupby('time_mandante')['resultado'].value_counts().unstack().fillna(0)
     print("Contagem de Resultados para Times Mandantes:")
     print(contagem_mandante)
 
-
-
     taxa_mandante = contagem_mandante.div(contagem_mandante.sum(axis=1), axis=0)
     print("Taxa de Resultados para Times Mandantes:")
     print(taxa_mandante)
-
 
     plt.figure(figsize=(12, 8))
     sns.heatmap(contagem_mandante, annot=True, cmap="Blues", fmt=".0f")
@@ -63,20 +61,10 @@ if st.button("Gerar gráfico de resultados dos times mandantes"):
     plt.show()
     st.pyplot()
 
-
-
-    from scipy.stats import chi2_contingency
+if st.button("Gerar gráfico para gráfico de barras que mostre a frequência de V/E/D para cada time mandante."):
 
     tabela_contingencia_mandante = pd.crosstab(dados_treino['time_mandante'], dados_treino['resultado'])
-
     chi2_mandante, p_mandante, _, _ = chi2_contingency(tabela_contingencia_mandante)
-    #print(f"Teste Qui-Quadrado para Time Mandante: Chi2 = {chi2_mandante:.2f}, p-valor = {p_mandante:.4f}")
-
-
-
-    # Supondo que a tabela de contingência já esteja criada como tabela_contingencia_mandante
-    # Se ainda não tiver, você pode criar assim:
-    # tabela_contingencia_mandante = pd.crosstab(dados_treino['time_mandante'], dados_treino['resultado'])
 
     # Plotando o gráfico de barras empilhadas
     tabela_contingencia_mandante.plot(kind='bar', stacked=True, figsize=(12, 8), colormap='viridis')
@@ -85,29 +73,12 @@ if st.button("Gerar gráfico de resultados dos times mandantes"):
     plt.title('Distribuição dos Resultados por Time Mandante')
     plt.xlabel('Time Mandante')
     plt.ylabel('Número de Partidas')
-
     # Alterando a legenda para refletir os valores de -1, 0, 1
     plt.legend(title='Resultado', labels=['Derrota do Time Mandante', 'Empate', 'Vitória do Time Mandante'], bbox_to_anchor=(1.05, 1), loc='upper left')
-    plt.xticks(rotation=90)  # Rotaciona os nomes dos times no eixo x para melhor leitura
+    plt.xticks(rotation=90)
     plt.tight_layout()
-
-    # Exibe o gráfico
     plt.show()
     st.pyplot()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 st.text("Jardel Ferreira - GRR20231834")
 
